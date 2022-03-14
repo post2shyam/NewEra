82deab6af552820295a75d0ed8e75ed1ba6e4eee
Shyam Grover <post2shyam@gmail.com>
2022-03-14 21:19:03 +0530

    livedata integration.

--- app/src/main/java/com/example/newera/MainActivityViewModel.kt
+++ app/src/main/java/com/example/newera/MainActivityViewModel.kt
@@ -1,10 +1,36 @@
 package com.example.newera
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel() : ViewModel() {
-    var number = 0
-    fun addNumber() {
-        number++
+    private lateinit var countDownTimer: CountDownTimer
+    private val _seconds = MutableLiveData<Int>()
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+    private val _isFinished = MutableLiveData<Boolean>()
+    val finished: LiveData<Boolean>
+        get() = _isFinished
+
+    fun startTimer() {
+        countDownTimer = object : CountDownTimer(10000, 1000) {
+            override fun onFinish() {
+                _isFinished.value = true
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                val timeRemainingInSeconds =
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)
+                _seconds.value = timeRemainingInSeconds.toInt()
+            }
+        }.start()
+    }
+
+    fun stopTimer() {
+        countDownTimer.cancel()
     }
 }
